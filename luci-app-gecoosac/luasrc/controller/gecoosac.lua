5d30b0a71ce471a09a4f30ad8599ade236774194
217heidai <11519717+217heidai@users.noreply.github.com>
2026-06-07 20:26:05 +0800

    Sun Jun  7 20:26:04 CST 2026

--- luci-app-gecoosac/luasrc/controller/gecoosac.lua
+++ luci-app-gecoosac/luasrc/controller/gecoosac.lua
@@ -1,8 +1,12 @@
 
 module("luci.controller.gecoosac", package.seeall)
 
+local fs = require "nixio.fs"
+local sys = require "luci.sys"
+local uci = require "luci.model.uci"
+
 function index()
-	if not nixio.fs.access("/etc/config/gecoosac") then
+	if not fs.access("/etc/config/gecoosac") then
 		return
 	end
 	local page
@@ -13,17 +17,11 @@ function index()
 end
 
 function act_status()
-	local e = {}
-	local binpath = "/usr/bin/gecoosac"
-	e = {
-		running = luci.sys.call("pgrep " .. binpath .. " >/dev/null") == 0
+	local cur = uci.cursor()
+	local enabled = cur:get("gecoosac", "config", "enabled") == "1"
+	local e = {
+		running = enabled and sys.call("/etc/init.d/gecoosac status >/dev/null 2>&1") == 0
 	}
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
-end
-
-function clear_upload()
-    local path = "/tmp/gecoosac/upload/"
-    luci.sys.call("rm -rf " .. path .. "/*")
-    luci.http.status(200, "OK")
 end
