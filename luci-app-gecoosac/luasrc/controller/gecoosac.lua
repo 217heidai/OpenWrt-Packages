3a2f16b71259c39ff5a5d50a65fae1c08aac9e1f
217heidai <11519717+217heidai@users.noreply.github.com>
2026-06-09 20:52:09 +0800

    Tue Jun  9 20:52:08 CST 2026

--- luci-app-gecoosac/luasrc/controller/gecoosac.lua
+++ luci-app-gecoosac/luasrc/controller/gecoosac.lua
@@ -1,9 +1,16 @@
-
 module("luci.controller.gecoosac", package.seeall)
 
 local fs = require "nixio.fs"
 local sys = require "luci.sys"
 local uci = require "luci.model.uci"
+
+local function service_running()
+	if not fs.access("/etc/init.d/gecoosac") then
+		return false
+	end
+
+	return sys.call("/etc/init.d/gecoosac status >/dev/null 2>&1") == 0
+end
 
 function index()
 	if not fs.access("/etc/config/gecoosac") then
@@ -20,7 +27,7 @@ function act_status()
 	local cur = uci.cursor()
 	local enabled = cur:get("gecoosac", "config", "enabled") == "1"
 	local e = {
-		running = enabled and sys.call("pidof gecoosac >/dev/null 2>&1") == 0
+		running = enabled and service_running()
 	}
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
